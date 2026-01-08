f64bf0c9aa0e06da5da56ac86bbdd6c3d1fdfd9a
rakoto.man <rakoto.man@smartone.ai>
2026-01-08 14:41:36 +0300

    front and back

--- users/views.py
+++ users/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import CustomUser
 
 def user_list(request):
-    return render(request, 'users/user_list.html')
+    users = CustomUser.objects.all()
+    return render(request, 'users/user_list.html', {'users': users})
