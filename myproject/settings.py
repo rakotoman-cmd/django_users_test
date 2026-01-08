dfc72a666cf72a1e1d0e4a5428eaf71fb959a639
rakoto.man <rakoto.man@smartone.ai>
2026-01-08 11:38:53 +0300

    just front

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+ 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
--- users/views.py
+++ users/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import CustomUser
 
 def user_list(request):
-    users = CustomUser.objects.all()
-    return render(request, 'users/user_list.html', {'users': users})
+    return render(request, 'users/user_list.html')
